d1746ae22c576e76f98c92793414e2e780411fff
shannongamby <shannongamby@hotmail.com>
2018-10-12 12:55:56 +0100

    gilded rose tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,11 +2,17 @@ require 'gilded_rose'
 
 describe GildedRose do
 
-  let(:item) { double :item }
-  subject { described_class.new([item]) }
+  let(:items) { double :items }
 
   it "should respond to #update_quality" do
-    allow(item).to receive(:update)
-    expect(subject.update_quality).to eq([item])
+    subject = GildedRose.new([items])
+    expect(subject).to respond_to(:update_quality)
+  end
+
+  it "passes when #update_quality is called and item receives #update" do
+    item = spy('item')
+    subject = GildedRose.new([item])
+    subject.update_quality
+    expect(item).to have_received(:update)
   end
 end
